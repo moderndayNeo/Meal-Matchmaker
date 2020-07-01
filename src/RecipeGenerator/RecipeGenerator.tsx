14a4143fcb837ced124d83ffc035b7166cc5c82d
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-07-01 11:14:15 +0100

    feat: Import Link in RecipePreview

--- src/RecipeGenerator/RecipeGenerator.tsx
+++ src/RecipeGenerator/RecipeGenerator.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './RecipeGenerator.css'
 import InfoIcon from './InfoIcon/InfoIcon'
+import RecipePreview from './RecipePreview/RecipePreview'
 
 export default function RecipeGenerator() {
     return (
         <div className="RecipeGenerator">
             <p className="header-text">Meal Matchmaker</p>
             <InfoIcon />
+            <RecipePreview />
         </div>
     )
 }
---
+++ src/RecipeGenerator/RecipePreview/RecipePreview.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './RecipePreview.css'
+import pancakes from '../../media/welcome-page-pancakes.jpg'
+import { Link } from 'react-router-dom'
+
+export default function RecipePreview() {
+    return (
+        <div className="RecipePreview">
+            <h1 className="preview-title">Roast Duck</h1>
+            <h4 className="preview-category">Duck</h4>
+            <img src={pancakes} alt=""/>
+            <Link to="/view-recipe">View Recipe</Link>
+        </div>
+    )
+}
