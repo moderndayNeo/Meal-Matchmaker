42ff338052c93e32ead4bc5b139256dd62d22a51
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-07-01 11:59:54 +0100

    feat: Style ViewRecipeButton

--- src/RecipeGenerator/RecipePreview/RecipePreview.tsx
+++ src/RecipeGenerator/RecipePreview/RecipePreview.tsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import './RecipePreview.css'
 import pancakes from '../../media/welcome-page-pancakes.jpg'
-import { Link } from 'react-router-dom'
-import IoMdArrowRoundForward from 'react-icons/io'
+import ViewRecipeButton from './ViewRecipeButton/ViewRecipeButton'
 
 export default function RecipePreview() {
     return (
         <div className="RecipePreview">
             <h1 className="preview-title">Roast Duck</h1>
             <h4 className="preview-category">Duck</h4>
-            <img src={pancakes} alt=""/>
-            <Link to="/view-recipe">View Recipe</Link>
-            
+            <img src={pancakes} alt="" />
+            <ViewRecipeButton />
         </div>
     )
 }
--- src/RecipeGenerator/RecipePreview/ViewRecipeButton/ViewRecipeButton.tsx
+++ src/RecipeGenerator/RecipePreview/ViewRecipeButton/ViewRecipeButton.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './ViewRecipeButton.css'
+import { Link } from 'react-router-dom'
+import {IoMdArrowRoundForward} from 'react-icons/io'
 
 export default function ViewRecipeButton() {
     return (
-        <div className="ViewRecipeButton">
-            ViewRecipeButton
-        </div>
+        <Link to="/view-recipe" className="ViewRecipeButton">
+            <p>View Recipe</p>
+            <IoMdArrowRoundForward className="icon" />
+        </Link>
     )
 }
