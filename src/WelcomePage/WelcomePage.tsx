93cad3b63d67144d4f2c4164cb89f12be92d06b4
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-07-01 10:20:49 +0100

    feat: Layout WelcomePage

--- src/WelcomePage/WelcomePage.tsx
+++ src/WelcomePage/WelcomePage.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './WelcomePage.css'
+import foodImage from '../media/healthy-meal.jpg'
 
 export default function WelcomePage() {
     return (
-        <div>
-            WelcomePage
+        <div className="WelcomePage">
+            <h1 className="title">Meal Matchmaker</h1>
+            <p className="subtext">Inspiration at the click of a button</p>
+            <img src={foodImage} alt="pancakes"/>
+            <button className="black-button">Give me a new recipe</button>
         </div>
     )
 }
