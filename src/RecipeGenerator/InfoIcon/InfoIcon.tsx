83cda379d1f5ae36e7e07bcdcd9d1d2c3f67d834
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-07-01 10:47:59 +0100

    feat: Add icon to RecipeGenerator

---
+++ src/RecipeGenerator/InfoIcon/InfoIcon.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './InfoIcon.css'
+import { BsInfoCircle } from 'react-icons/bs'
+
+export default function InfoIcon() {
+    return (
+        <div className="InfoIcon">
+            <BsInfoCircle />
+        </div>
+    )
+}
--- src/RecipeGenerator/RecipeGenerator.tsx
+++ src/RecipeGenerator/RecipeGenerator.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './RecipeGenerator.css'
+import InfoIcon from './InfoIcon/InfoIcon'
 
 export default function RecipeGenerator() {
     return (
         <div className="RecipeGenerator">
-            RecipeGenerator
+            <p className="header-text">Meal Matchmaker</p>
+            <InfoIcon />
         </div>
     )
 }
