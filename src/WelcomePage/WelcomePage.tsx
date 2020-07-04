110d66132540beae235117ba24518d0217f940af
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-07-04 09:02:18 +0100

    feat: Display coffee cup image on WelcomePage

--- src/WelcomePage/WelcomePage.tsx
+++ src/WelcomePage/WelcomePage.tsx
@@ -1,16 +1,14 @@
 import * as React from 'react'
 import './WelcomePage.css'
-import foodImage from '../media/healthy-meal.jpg'
 import { Link } from 'react-router-dom'
-import arrow from '../media/Icons_right_arrow_dark.svg'
+import coffee from '../media/welcome_page_coffee.svg'
 
 export default function WelcomePage() {
     return (
         <div className="WelcomePage">
             <h1 className="title">Meal Matchmaker</h1>
             <p className="subtext">Inspiration at the click of a button</p>
-            <img src={foodImage} alt="man hugging coffee cup"/>
-            <img src={arrow} alt=""/>
+            <img src={coffee} alt="man hugging coffee cup"/>
             <Link className="black-button" to="/recipe-generator">Find My Match!</Link>
         </div>
     )
