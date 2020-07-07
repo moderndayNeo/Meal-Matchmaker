e76cdb19f778f4615d937296d20d92e12d08df01
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-07-07 09:11:34 +0100

    feat: Link infoIcon to Contact page

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import './Navbar.css'
 import infoIcon from '../media/Icons_info_dark.svg'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
     return (
         <div className="Navbar">
             <p className="title">Meal Matchmaker</p>
-            <img className="infoIcon" src={infoIcon} alt="info icon" />
+            <Link to="/contact">
+                <img className="infoIcon" src={infoIcon} alt="info icon" />
+            </Link>
         </div>
     )
 }
