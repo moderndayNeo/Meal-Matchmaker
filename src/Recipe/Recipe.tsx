a70326c7b1cc3315e60f7b905d84ff94da207be4
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-07-07 13:59:31 +0100

    refactor: Re-size Recipe nav bar & BackButton

--- src/Recipe/Recipe.tsx
+++ src/Recipe/Recipe.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Recipe.css'
 import RecipeDetails from './RecipeDetails/RecipeDetails'
 import { IMeal } from '../IMeal'
-import backArrow from '../media/Icons_left_arrow_white.svg'
+import { ReactComponent as BackButton } from '../media/Icons_left_arrow_white.svg'
 import { useHistory } from 'react-router-dom'
 
 export type RecipeProps = {
@@ -19,12 +19,7 @@ export default function Recipe({ meal }: RecipeProps) {
     return (
         <div className="Recipe">
             <nav className="overlay">
-                <img
-                    src={backArrow}
-                    alt="back arrow"
-                    className="back-arrow"
-                    onClick={goBack}
-                />
+                <BackButton className="back-button" onClick={goBack} />
             </nav>
             <img src={meal.strMealThumb} className="recipe-image" alt="meal" />
             <RecipeDetails meal={meal} />
