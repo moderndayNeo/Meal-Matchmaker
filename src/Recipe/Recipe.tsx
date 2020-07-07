ba25459ff2e64537026ff0be8f21e18f4ce7aa29
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-07-07 08:47:05 +0100

    feat: Create recipe overlay

--- src/Recipe/Recipe.tsx
+++ src/Recipe/Recipe.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Recipe.css'
 import RecipeDetails from './RecipeDetails/RecipeDetails'
 import { IMeal } from '../IMeal'
+import backArrow from '../media/Icons_left_arrow_white.svg'
 
 export type RecipeProps = {
     meal: IMeal
@@ -10,6 +11,9 @@ export type RecipeProps = {
 export default function Recipe({ meal }: RecipeProps) {
     return (
         <div className="Recipe">
+            <nav className="overlay">
+            <img src={backArrow} alt="back arrow" className="back-arrow"/>
+            </nav>
             <img src={meal.strMealThumb} className="recipe-image" alt="meal" />
             <RecipeDetails meal={meal} />
         </div>
