d85dd0ebcc2c19721864ed1922177bc537686a13
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-07-07 10:25:51 +0100

    refactor: Position elements on RecipeGenerator

--- src/RecipeGenerator/NewRecipeButton/NewRecipeButton.tsx
+++ src/RecipeGenerator/NewRecipeButton/NewRecipeButton.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import './NewRecipeButton.css'
-// import { MdSwapVerticalCircle } from 'react-icons/md'
 import regenerateIcon from '../../media/Icons_regenerate_dark.svg'
-
 
 export default function NewRecipeButton() {
     return (
         <div className="NewRecipeButton generate-button">
             <div className="text-container">
                 <p className="text">Generate Again</p>
-                {/* <MdSwapVerticalCircle /> */}
-                <img src={regenerateIcon} alt="regenerate icon"/>
+                <img src={regenerateIcon} alt="regenerate icon" />
             </div>
         </div>
     )
