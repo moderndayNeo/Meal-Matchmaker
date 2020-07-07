56d17d8e529c2711611492482c302ba817dee788
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-07-07 09:35:26 +0100

    feat: Add functionality to ContactPage back arrow

--- src/ContactPage/ContactPage.tsx
+++ src/ContactPage/ContactPage.tsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import './ContactPage.css'
+import backArrow from '../media/Icon_left_arrow.svg'
+import { useHistory } from 'react-router-dom'
 
 export default function ContactPage() {
+
+    const history = useHistory()
+
+    const goBack = (): void => {
+        history.goBack()
+    }
+
     return (
         <div className="ContactPage">
-            ContactPage
+            <div className="contact-nav">
+                <img
+                    src={backArrow}
+                    alt="back arrow"
+                    className="back-arrow"
+                    onClick={goBack}
+                />
+            </div>
+
+            <div className="main-wrapper">
+                <main>
+                    <h1>Our Story</h1>
+                    <p className="summary">
+                        Design: Putra Nasara Developer: Adam Zdrzalka We are
+                        growing digital team who focus on UI, UX & web
+                        development. We love to make enjoyable website & apps.
+                        Contact us to say Hi or to ask about projects.
+                    </p>
+                </main>
+            </div>
         </div>
     )
 }
