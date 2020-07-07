eb8cfd3499a16521c86a4719e7a6fa0f902b67ed
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-07-07 09:09:02 +0100

    feat: Add Back Button to Recipe page

--- src/Recipe/Recipe.tsx
+++ src/Recipe/Recipe.tsx
@@ -3,16 +3,30 @@ import './Recipe.css'
 import RecipeDetails from './RecipeDetails/RecipeDetails'
 import { IMeal } from '../IMeal'
 import backArrow from '../media/Icons_left_arrow_white.svg'
+import { useHistory } from 'react-router-dom'
 
 export type RecipeProps = {
     meal: IMeal
 }
 
+
 export default function Recipe({ meal }: RecipeProps) {
+    
+    const history = useHistory()
+    
+    const goBack = (): void => {
+        history.goBack()
+    }
+
     return (
         <div className="Recipe">
             <nav className="overlay">
-            <img src={backArrow} alt="back arrow" className="back-arrow"/>
+                <img
+                    src={backArrow}
+                    alt="back arrow"
+                    className="back-arrow"
+                    onClick={goBack}
+                />
             </nav>
             <img src={meal.strMealThumb} className="recipe-image" alt="meal" />
             <RecipeDetails meal={meal} />
