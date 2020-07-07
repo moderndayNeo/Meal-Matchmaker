325995bba316701197ec2daab7e9534205de24ba
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-07-07 12:44:34 +0100

    refactor: Reposition recipe Instructions

--- src/Recipe/RecipeDetails/Instructions/Instructions.tsx
+++ src/Recipe/RecipeDetails/Instructions/Instructions.tsx
@@ -8,14 +8,14 @@ interface InstructionsProps {
 
 export default function Instructions({ meal }: InstructionsProps) {
     return (
-        <section className="instructions">
-            <h3 className="instructions-title">Instructions</h3>
+        <section className="Instructions">
+            <h3 className="title">Instructions</h3>
 
             {meal.strInstructions
                 .split('\n')
                 .map((instruction: string, index: number) => (
                     <div key={index} className="instruction-and-number">
-                        <p className="instruction-number">{index + 1}. </p>
+                        <p className="number">{index + 1}. </p>
                         <p className="instruction">{instruction}</p>
                     </div>
                 ))}
