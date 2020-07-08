27349e0cf416f02622ef03406b6d0bc6b3212053
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-07-08 08:20:28 +0100

    feat: Add content to Header

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Header.css'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
     return (
         <div className="Header">
-            Header
+            <h1 className="title">Meal Matchmaker</h1>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/contact">Contact</Link>
+            </nav>
         </div>
     )
 }
