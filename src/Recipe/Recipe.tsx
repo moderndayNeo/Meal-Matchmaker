34271150405e1db31c69dd516c7295b62e474ba5
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-07-08 10:01:24 +0100

    feat: Create RecipeComponentsDesktop grouping

--- src/Recipe/Recipe.tsx
+++ src/Recipe/Recipe.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './Recipe.css'
 import {IMeal  } from '../IMeal'
-
 import RecipeComponentsMobile from './RecipeComponentsMobile/RecipeComponentsMobile'
+import RecipeComponentsDesktop from './RecipeComponentsDesktop/RecipeComponentsDesktop'
 
 interface RecipeProps {
     meal: IMeal
@@ -13,7 +13,7 @@ export default function Recipe({ meal }: RecipeProps) {
     return (
         <div className="Recipe">
             <RecipeComponentsMobile meal={meal} />
-       
+            <RecipeComponentsDesktop meal={meal} />
         </div>
     )
 }
---
+++ src/Recipe/RecipeComponentsDesktop/RecipeComponentsDesktop.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './RecipeComponentsDesktop.css'
+import {IMeal} from '../../IMeal'
+
+interface RecipeComponentsDesktopProps {
+    meal: IMeal
+}
+
+export default function RecipeComponentsDesktop({meal}: RecipeComponentsDesktopProps) {
+    return (
+        <div className="RecipeComponentsDesktop">
+            
+        </div>
+    )
+}
