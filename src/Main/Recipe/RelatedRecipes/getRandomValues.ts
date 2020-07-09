280366567c5e033a90cc06a39764b7f437f7e333
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-07-09 20:25:08 +0100

    feat: Get 3 random related recipes from the API

---
+++ src/Main/Recipe/RelatedRecipes/getRandomValues.ts
@@ -0,0 +1,11 @@
+export default function getRandomisedArray(length: number) {
+    const helper: number[] = []
+
+    function getRandomValue() {
+        const value = Math.floor(Math.random() * length)
+        if (!helper.includes(value)) return helper.push(value)
+        getRandomValue()
+    }
+    while (helper.length < 3) getRandomValue()
+    return helper
+}
