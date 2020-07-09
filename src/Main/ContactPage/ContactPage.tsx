ede4ffee7280fff7fc142d6686b67904eeb6d328
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-07-09 21:16:12 +0100

    refactor: Update ContactPage summary

--- src/Main/ContactPage/ContactPage.tsx
+++ src/Main/ContactPage/ContactPage.tsx
@@ -25,27 +25,26 @@ export default function ContactPage() {
 
             <div className="main-wrapper">
                 <main>
-                    <h1>Our Team</h1>
-                    <div className="summary">
-                        <p>
-                            We are growing digital team who focus on UI, UX &
-                            web development. We love to make enjoyable websites
-                            & apps. Contact us to say Hi or to ask about
-                            projects.
-                        </p>
-                        <TeamMember
-                            title="Putra Narassa (UI Designer)"
-                            github=""
-                            facebook="https://www.facebook.com/putra.narassa"
-                            linkedin=""
-                        />
-                        <TeamMember
-                            title="Adam Zdrzalka (Web Developer)"
-                            github="https://github.com/moderndayNeo?tab=repositories"
-                            facebook=""
-                            linkedin="https://www.linkedin.com/in/adamzdrzalka/"
-                        />
-                    </div>
+                    <h2>Our Team</h2>
+                    <p className="summary">
+                        Meal Matchmaker is a random recipe generator so you
+                        never run out of ideas in the kitchen. Meal Matchmaker
+                        is made by Putra and Adam. Feel free to contact us to
+                        talk about projects or discuss UX and web-mobile
+                        development.
+                    </p>
+                    <TeamMember
+                        title="Putra Narassa (UI Designer)"
+                        github=""
+                        facebook="https://www.facebook.com/putra.narassa"
+                        linkedin=""
+                    />
+                    <TeamMember
+                        title="Adam Zdrzalka (Web Developer)"
+                        github="https://github.com/moderndayNeo?tab=repositories"
+                        facebook=""
+                        linkedin="https://www.linkedin.com/in/adamzdrzalka/"
+                    />
 
                     <img
                         src={developers}
