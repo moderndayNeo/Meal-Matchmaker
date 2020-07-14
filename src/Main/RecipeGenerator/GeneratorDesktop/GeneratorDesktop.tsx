e3e614a596746f5a28436b8776a462ca6b94f104
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-07-14 17:01:55 +0100

    feat: Begin styling GeneratorDesktop

--- src/Main/RecipeGenerator/GeneratorDesktop/GeneratorDesktop.tsx
+++ src/Main/RecipeGenerator/GeneratorDesktop/GeneratorDesktop.tsx
@@ -1,16 +1,31 @@
 import React from 'react'
 import './GeneratorDesktop.css'
 import { IMeal } from '../../../IMeal'
+import NewRecipeButton from '../NewRecipeButton/NewRecipeButton'
+import ViewRecipeButton from '../RecipePreview/ViewRecipeButton/ViewRecipeButton'
 
 interface GeneratorDesktopProps {
     meal: IMeal
     onClick: () => void
 }
 
-export default function GeneratorDesktop({meal, onClick}: GeneratorDesktopProps) {
+export default function GeneratorDesktop({
+    meal,
+    onClick,
+}: GeneratorDesktopProps) {
     return (
         <div className="GeneratorDesktop">
-            
+            <div className="title-and-buttons">
+                <h1 className="title">{meal.strMeal}</h1>
+                <div className="buttons">
+                    <NewRecipeButton onClick={onClick} />
+                    <ViewRecipeButton meal={meal} />
+                </div>
+            </div>
+
+            <div className="image-container">
+                <img src={meal.strMealThumb} alt="meal" />
+            </div>
         </div>
     )
 }
