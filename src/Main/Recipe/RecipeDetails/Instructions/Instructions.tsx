891358e53c26573a17f9635d2fbf9fbe522b9c03
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-07-15 16:12:43 +0100

    chore: Remove comments from Instructions

--- src/Main/Recipe/RecipeDetails/Instructions/Instructions.tsx
+++ src/Main/Recipe/RecipeDetails/Instructions/Instructions.tsx
@@ -10,15 +10,6 @@ export default function Instructions({ meal }: InstructionsProps) {
     return (
         <section className="Instructions">
             <h3 className="title">Instructions</h3>
-
-            {/* {meal.strInstructions
-                .split('\n')
-                .map((instruction: string, index: number) => (
-                    <div key={index} className="instruction-and-number">
-                        <p className="number">{index + 1}. </p>
-                        <p className="instruction">{instruction}</p>
-                    </div>
-                ))} */}
             {meal.strInstructions
                 .split('\n')
                 .map((instruction: string, index: number) => (
