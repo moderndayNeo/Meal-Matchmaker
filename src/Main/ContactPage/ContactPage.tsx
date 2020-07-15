83f2dcdc1476a9496633d46c7192d69dcae82d82
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-07-15 09:06:25 +0100

    feat: Continue ContactPage desktop styling

--- src/Main/ContactPage/ContactPage.tsx
+++ src/Main/ContactPage/ContactPage.tsx
@@ -26,14 +26,19 @@ export default function ContactPage() {
             <div className="main-wrapper">
                 <main>
                     <h2 className="title">Our Team</h2>
-                    <p className="summary">
-                        Meal Matchmaker is a random recipe generator so you
-                        never run out of ideas in the kitchen. After discussing
-                        ideas on a UI Forum, Putra and Adam teamed up to build
-                        the app; overcoming timezones and lockdown. Feel free to
-                        contact us for work or to discuss UX and web-mobile
-                        development.
-                    </p>
+                    <div className="summary">
+                        <p>
+                            Meal Matchmaker is a random recipe generator so you
+                            never run out of ideas in the kitchen. After
+                            discussing ideas on a UI Forum, Putra and Adam
+                            teamed up to build the app; overcoming timezones and
+                            lockdown.
+                        </p>
+                        <p>
+                            Feel free to contact us for work or to discuss UX
+                            and web-mobile development.
+                        </p>
+                    </div>
                     <div className="team-members">
                         <TeamMember
                             name="Putra Narassa (UI Designer)"
