0ddc8dcf607b1bfdd6cf585c57ee4106ff3823b7
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-07-16 13:24:28 +0100

    refactor: Move ContactPageImage to own file

--- src/Main/ContactPage/ContactPage.tsx
+++ src/Main/ContactPage/ContactPage.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import './ContactPage.css'
 import backArrow from '../../media/Icon_left_arrow.svg'
 import { useHistory } from 'react-router-dom'
-import developers from '../../media/developers-mobile.jpeg'
-import developersDesktopImg from '../../media/developers-desktop.jpg'
 import TeamMember from './TeamMember/TeamMember'
+import ContactPageSummary from './ContactPageSummary/ContactPageSummary'
+import ContactPageImage from './ContactPageImage/ContactPageImage'
 
 export default function ContactPage() {
     const history = useHistory()
@@ -27,19 +27,7 @@ export default function ContactPage() {
             <div className="main-wrapper">
                 <main>
                     <h2 className="title">Our Team</h2>
-                    <div className="summary">
-                        <p>
-                            Meal Matchmaker is a random recipe generator so you
-                            never run out of ideas in the kitchen. After
-                            discussing ideas on a UI Forum, Putra and Adam
-                            teamed up to build the app; overcoming timezones and
-                            lockdown.
-                        </p>
-                        <p>
-                            Feel free to contact us for work or to discuss UX
-                            and web-mobile development.
-                        </p>
-                    </div>
+                    <ContactPageSummary />
                     <div className="team-members">
                         <TeamMember
                             name="Putra Narassa (UI Designer)"
@@ -56,17 +44,7 @@ export default function ContactPage() {
                             mailto="adamzdrzalka@outlook.com"
                         />
                     </div>
-
-                    <img
-                        src={developers}
-                        alt="development team"
-                        className="developer-image mobile-image"
-                    />
-                    <img
-                        src={developersDesktopImg}
-                        alt="development team"
-                        className="developer-image desktop-image"
-                    />
+                    <ContactPageImage />
                 </main>
             </div>
         </div>
---
+++ src/Main/ContactPage/ContactPageSummary/ContactPageSummary.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './ContactPageSummary.css'
+
+export default function ContactPageSummary() {
+    return (
+        <div className="ContactPageSummary">
+                        <p>
+                            Meal Matchmaker is a random recipe generator so you
+                            never run out of ideas in the kitchen. After
+                            discussing ideas on a UI Forum, Putra and Adam
+                            teamed up to build the app; overcoming timezones and
+                            lockdown.
+                        </p>
+                        <p>
+                            Feel free to contact us for work or to discuss UX
+                            and web-mobile development.
+                        </p>
+                    </div>
+    )
+}
