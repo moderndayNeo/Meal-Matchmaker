459a1a176f8d7e234e24d512c1362337f9f56ba0
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-07-16 16:13:39 +0100

    refactor: Re-size Recipe font on desktop

--- src/Main/ContactPage/ContactPageSummary/ContactPageSummary.tsx
+++ src/Main/ContactPage/ContactPageSummary/ContactPageSummary.tsx
@@ -4,17 +4,16 @@ import './ContactPageSummary.css'
 export default function ContactPageSummary() {
     return (
         <div className="ContactPageSummary">
-                        <p>
-                            Meal Matchmaker is a random recipe generator so you
-                            never run out of ideas in the kitchen. After
-                            discussing ideas on a UI Forum, Putra and Adam
-                            teamed up to build the app; overcoming timezones and
-                            lockdown.
-                        </p>
-                        <p>
-                            Feel free to contact us for work or to discuss UX
-                            and web-mobile development.
-                        </p>
-                    </div>
+            <p>
+                Meal Matchmaker is a random recipe generator so you never run
+                out of ideas in the kitchen. After discussing the idea on a UI
+                Forum, Adam and Putra teamed up to build the app; coronavirus
+                and timezone differences, be damned.
+            </p>
+            <p>
+                Feel free to contact us for work or simply to discuss UX and
+                web-mobile development.
+            </p>
+        </div>
     )
 }
