751a7dcacb68a73f6d3f793e5b613833c67ac439
Adam Zdrzalka <Adam.JZ@outlook.com>
2021-02-06 22:41:42 +0000

    chore: Attach middlware to node server

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,1 +1,14 @@
 const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+}
+
+app.use(cors(corsOptions))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
