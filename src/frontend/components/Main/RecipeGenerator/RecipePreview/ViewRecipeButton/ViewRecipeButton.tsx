88577aa42625070d764f1761911e04320b9415ae
Adam Zdrzalka <Adam.JZ@outlook.com>
2021-02-13 14:50:15 +0000

    feat: Link ViewRecipeButton to the recipe

--- src/frontend/components/Main/RecipeGenerator/RecipePreview/ViewRecipeButton/ViewRecipeButton.tsx
+++ src/frontend/components/Main/RecipeGenerator/RecipePreview/ViewRecipeButton/ViewRecipeButton.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './ViewRecipeButton.css'
 import { IoMdArrowRoundForward } from 'react-icons/io'
+import { useHistory } from 'react-router-dom'
+
 export default function ViewRecipeButton() {
+    const history = useHistory()
+
     return (
-        <button className="ViewRecipeButton">
+        <button
+            onClick={(e) => history.push('/recipe')}
+            className="ViewRecipeButton"
+        >
             <p>View Recipe</p>
             <IoMdArrowRoundForward className="right-arrow" />
         </button>
