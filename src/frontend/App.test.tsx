17c89deffed945ac5167377eaca827c77f6f84ae
Adam Zdrzalka <Adam.JZ@outlook.com>
2021-02-14 10:50:11 +0000

    test: Ensure NewRecipeButton renders correctly

--- src/frontend/App.test.tsx
+++ src/frontend/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './components/App';
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from './components/App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+it('renders without crashing', () => {
+    const { getByRole } = render(<App />)
+    const newRecipeButton = getByRole('link', { name: /find my match!/i })
+    expect(newRecipeButton).toBeTruthy()
+})
