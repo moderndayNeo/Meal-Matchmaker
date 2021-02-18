ba31f3a9050e8540746a05661edb05e0713adbf3
Adam Zdrzalka <Adam.JZ@outlook.com>
2021-02-18 15:34:36 +0000

    feat: Add form to user login page

--- src/frontend/components/Auth/Login/Login.tsx
+++ src/frontend/components/Auth/Login/Login.tsx
@@ -1,11 +1,47 @@
 import React from 'react'
 import './Login.css'
-import { useHistory } from 'react-router-dom'
+import { useHistory, Link } from 'react-router-dom'
 
 export default function Login() {
     const [username, setUsername] = React.useState<string>('')
     const [password, setPassword] = React.useState<string>('')
     const history = useHistory()
 
-    return <div>Login page</div>
+    const handleSubmit = (e: any) => {
+        e.preventDefault()
+
+        // attempt to login the user, handle response
+    }
+
+    return (
+        <div className="Login">
+            <h1 className="title">Sign in to your account</h1>
+            <form>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <br />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    disabled
+                />
+
+                <button className="generate-button" onClick={handleSubmit}>
+                    Log In
+                </button>
+            </form>
+            <hr />
+            --- Or ---
+            <hr />
+            <Link className="generate-button" to="/sign-up">
+                Return to Sign Up
+            </Link>
+        </div>
+    )
 }
