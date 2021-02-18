6a063c9798dd6389f29a07fbbc3b75f441098135
Adam Zdrzalka <Adam.JZ@outlook.com>
2021-02-18 15:18:57 +0000

    feat: Add controller action to return all users

--- src/backend/controllers/users.controller.js
+++ src/backend/controllers/users.controller.js
@@ -1,5 +1,15 @@
 const User = require('../models').User
 
+exports.findAll = (req, res) => {
+    User.findAll()
+        .then((data) => res.send(data))
+        .catch((err) => {
+            res.status(404).send({
+                message: err.message || `No users found`,
+            })
+        })
+}
+
 exports.create = (req, res) => {
     if (!req.body.username) {
         res.status(400).send({
@@ -11,7 +21,7 @@ exports.create = (req, res) => {
     const user = {
         username: req.body.username,
     }
-    
+
     User.create(user)
         .then((data) => res.send(data))
         .catch((error) => {
--- src/backend/routes/user.routes.js
+++ src/backend/routes/user.routes.js
@@ -4,9 +4,7 @@ const users = require('../controllers/users.controller') // user controller
 module.exports = (app) => {
     router.post('/', users.create) // call the relevant users controller action
 
-    router.get('/', (req, res) => {
-        res.json({ page: 'user' })
-    })
+    router.get('/', users.findAll)
 
     app.use('/api/users', router)
 }
