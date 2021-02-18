0fe11af022a03eaf680aa5e1d9a5c739e4a037a0
Adam Zdrzalka <Adam.JZ@outlook.com>
2021-02-18 17:36:52 +0000

    feat: Add Profile header to Profile page

--- src/frontend/components/Profile/Profile.tsx
+++ src/frontend/components/Profile/Profile.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import './Profile.css'
 import ReturnNavbar from '../common/ReturnNavbar'
 
 export default function Profile() {
     return (
-        <div>
+        <div className="Profile">
             <ReturnNavbar />
+            <h1 className="title">Profile</h1>
         </div>
     )
 }
