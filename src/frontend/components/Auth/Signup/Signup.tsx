0dea5dfea45b379df1ff79a9453f223afb3483dd
Adam Zdrzalka <Adam.JZ@outlook.com>
2021-02-18 15:24:29 +0000

    feat: Re-route user to generator page on signup

--- src/frontend/components/Auth/Signup/Signup.tsx
+++ src/frontend/components/Auth/Signup/Signup.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import './Signup.css'
 import APIUtil from '../../../util/api.utils'
+import { useHistory } from 'react-router-dom'
 
 export default function Signup() {
     const [username, setUsername] = React.useState<string>('')
     const [password, setPassword] = React.useState<string>('')
+    const history = useHistory()
 
     const handleSubmit = (e: any) => {
         e.preventDefault()
-        APIUtil.createUser({username}).then((res) => handleResponse(res))
+        APIUtil.createUser({ username }).then((res) => handleResponse(res))
     }
 
     interface ResObject {
@@ -17,11 +19,10 @@ export default function Signup() {
 
     const handleResponse = (res: ResObject) => {
         if (res.status === 200) {
-            console.log('user created')
-            // handle success
+            // handle success - login user and re-route to recipe page or profile page
+            history.push('/recipe-generator')
         } else {
-            console.log('error in creating the user')
-            // handle error
+            console.log(res)
         }
     }
 
