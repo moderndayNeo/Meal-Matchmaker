26e4665f3de7cf7cd8bf9dc29d4eb3774c8d4758
Adam Zdrzalka <Adam.JZ@outlook.com>
2021-02-18 14:57:14 +0000

    feat: Create user signup form

--- src/frontend/components/Auth/Signup/Signup.tsx
+++ src/frontend/components/Auth/Signup/Signup.tsx
@@ -2,9 +2,40 @@ import React from 'react'
 import './Signup.css'
 
 export default function Signup() {
+    const [username, setUsername] = React.useState('')
+    const [password, setPassword] = React.useState('')
+
+    const handleSubmit = () => {
+        // create user
+    }
+
     return (
         <div className="Signup">
-            Signup Page
+            <h1 className="title">Create your account</h1>
+            <p className="subtext">
+                Sign up to save your favourite recipes and add your own
+            </p>
+
+            <form>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <br />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    disabled
+                />
+
+                <button className="generate-button" onClick={handleSubmit}>
+                    Sign Up
+                </button>
+            </form>
         </div>
     )
 }
