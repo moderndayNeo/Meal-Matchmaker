dae33e55ca5d99c4303cec3600a07f52d8ff0e28
Adam Zdrzalka <Adam.JZ@outlook.com>
2021-02-19 10:28:01 +0000

    feat: Add setAuthToken to http-common file

--- src/frontend/util/http-common.js
+++ src/frontend/util/http-common.js
@@ -1,10 +1,16 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://localhost:3001/api',
+    baseURL: 'http://localhost:3001/api', // Could use process.env.PORT here
     headers: {
         'Content-type': 'application/json',
     },
 })
 
-// Could use process.env.PORT here
+export const setAuthToken = (token) => {
+    if (token) {
+        axios.defaults.headers.common['Authorization'] = token
+    } else {
+        delete axios.defaults.headers.common['Authorization']
+    }
+}
