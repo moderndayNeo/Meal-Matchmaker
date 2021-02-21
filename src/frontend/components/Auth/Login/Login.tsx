c034dfa44d2b08cce30ab924d81605cb2ed3f8bd
Adam Zdrzalka <Adam.JZ@outlook.com>
2021-02-21 17:41:20 +0000

    task: Remove unused code from Login component

--- src/frontend/components/Auth/Login/Login.tsx
+++ src/frontend/components/Auth/Login/Login.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './Login.css'
-import { useHistory, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { AuthContext } from '../../../context/AuthContext'
 
 export default function Login() {
+    const { signIn } = React.useContext(AuthContext)
+
     const [username, setUsername] = React.useState<string>('')
     const [password, setPassword] = React.useState<string>('')
-    const history = useHistory()
-    const { signIn } = React.useContext(AuthContext)
 
     const handleSubmit = (e: any) => {
         e.preventDefault()
@@ -31,7 +31,6 @@ export default function Login() {
                     placeholder="Password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
-                    // disabled
                 />
 
                 <button className="generate-button" onClick={handleSubmit}>
