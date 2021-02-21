8f45e7f07579984e7b5856b09fb734537cb24c38
Adam Zdrzalka <Adam.JZ@outlook.com>
2021-02-21 21:10:29 +0000

    feat: Link home icon to /login if not signed in

--- src/frontend/components/Navbar/Navbar.tsx
+++ src/frontend/components/Navbar/Navbar.tsx
@@ -3,8 +3,10 @@ import './Navbar.css'
 import infoIcon from '../../media/Icons_info_dark.svg'
 import homeIcon from '../../media/icons_home.svg'
 import { Link } from 'react-router-dom'
+import { AuthContext } from '../../context/AuthContext'
 
 export default function Navbar() {
+    const { isLoggedIn } = React.useContext(AuthContext)
     return (
         <div className="Navbar">
             <Link to="/">
@@ -13,9 +15,13 @@ export default function Navbar() {
 
             <div className="navbar-icons">
                 <Link to="/contact">
-                    <img className="icon infoIcon" src={infoIcon} alt="info icon" />
+                    <img
+                        className="icon infoIcon"
+                        src={infoIcon}
+                        alt="info icon"
+                    />
                 </Link>
-                <Link to="/profile">
+                <Link to={isLoggedIn ? '/profile' : '/login'}>
                     <img
                         className="icon homeIcon"
                         src={homeIcon}
