4b0f6edf0460f98a109ca7465ab68417a70f982e
Adam Zdrzalka <Adam.JZ@outlook.com>
2021-02-21 17:39:25 +0000

    task: Create AuthRoute util route

--- src/frontend/util/route_utils.js
+++ src/frontend/util/route_utils.js
@@ -1,32 +1,31 @@
 import React from 'react'
 import { Redirect, Route } from 'react-router-dom'
-const loggedIn = true // refactor this to check if user if loggedIn
+import { AuthContext } from '../context/AuthContext'
 
-export const ProtectedRoute = ({ exact, path, component: Component }) => (
-    <Route
-        path={path}
-        exact={exact}
-        render={(props) =>
-            loggedIn ? (
-                <Component {...props} />
-            ) : (
-                // Redirect to the login page if the user is already authenticated
-                <Redirect to="/" />
-            )
-        }
-    />
-)
+export const ProtectedRoute = ({ exact, path, component: Component }) => {
+    const { isLoggedIn } = React.useContext(AuthContext)
 
-/*
-// Passed in from parent component or from mapStateToProps
-const Auth = ({ component: Component, path, loggedIn, exact }) => (
-  <Route path={path} exact={exact} render={(props) => (
-    !loggedIn ? (
-      <Component {...props} />
-    ) : (
-        // Redirect to the tweets page if the user is authenticated
-      <Redirect to="/tweets" />
+    return (
+        <Route
+            path={path}
+            exact={exact}
+            render={(props) =>
+                isLoggedIn ? <Component {...props} /> : <Redirect to="/" />
+            }
+        />
     )
-  )} />
-);
-*/
+}
+
+export const AuthRoute = ({ exact, path, component: Component }) => {
+    const { isLoggedIn } = React.useContext(AuthContext)
+
+    return (
+        <Route
+            path={path}
+            exact={exact}
+            render={(props) =>
+                isLoggedIn ? <Redirect to="/" /> : <Component {...props} />
+            }
+        />
+    )
+}
