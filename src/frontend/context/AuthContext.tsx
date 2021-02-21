5c7f2055e6ef9b2497bd379df4df2aec7455373f
Adam Zdrzalka <Adam.JZ@outlook.com>
2021-02-21 18:22:14 +0000

    refactor: Use jwt-decode in AuthContext

--- src/frontend/context/AuthContext.tsx
+++ src/frontend/context/AuthContext.tsx
@@ -1,7 +1,7 @@
 import React, { createContext, useEffect, useReducer } from 'react'
 import APIUtil from '../util/api.utils'
 import { setAuthToken } from '../util/http-common'
-
+import jwt_decode, { JwtPayload } from 'jwt-decode'
 export const AuthContext = createContext<any>({})
 const LOCAL_STORAGE_AUTH_TOKEN = 'auth_jwt'
 
@@ -54,43 +54,48 @@ const Auth: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 
     useEffect(() => {
         const token = localStorage.getItem(LOCAL_STORAGE_AUTH_TOKEN)
-        if (!token) {
-            console.log('No token in localStorage')
-            return
+        if (!token) return console.log('No token in localStorage')
+
+        const decodedUser: any = jwt_decode<JwtPayload>(token)
+        if (decodedUser) {
+            dispatch({
+                type: 'SIGN IN',
+                user: { id: decodedUser.id, username: decodedUser.username },
+                token,
+            })
+        } else {
+            console.log('code expired')
         }
 
-        // if valid get back user from BE, update token in state, set token in LS.
-        setAuthToken(token) // add auth token to axios header
-        APIUtil.checkToken() // make a get request to see if BE verifies it
-            .then((user) => {
-                dispatch({ type: 'SIGN IN', user, token })
-            })
-            .catch((err) => console.log(err))
+        // setAuthToken(token)
+        // APIUtil.checkToken()
+        //     .then((user) => {
+        //         dispatch({ type: 'SIGN IN', user, token })
+        //     })
+        //     .catch((err) => console.log(err))
     }, [])
 
-    /*
-    make login page work
-    login with guest account
-    call the login function from the login page
-    check that JWT is held in LS
-    if user is logged in, we should we Welcome, Username in the navbar
-    */
+    const extractData = (response: any) => ({
+        user: response.data.user,
+        token: response.data.token,
+    })
 
     const authContext = {
         signIn: async ({ username, password }: LoginProps) => {
-            try {
-                const response: any = await APIUtil.signInUser({
-                    password,
-                    username,
-                })
+            if (!username || !password) return
 
-                const { token, id } = await response.data
+            try {
+                const { token, user } = await APIUtil.signInUser({
+                    username,
+                    password,
+                }).then(extractData)
+
                 setAuthToken(token)
                 localStorage.setItem(LOCAL_STORAGE_AUTH_TOKEN, token)
                 dispatch({
                     type: 'SIGN IN',
                     token,
-                    user: { username, id },
+                    user,
                 })
                 console.log('login successful')
             } catch (err) {
@@ -114,29 +119,3 @@ const Auth: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 }
 
 export default Auth
-
-// imports
-// declare constants
-// delcare and extend interfaces
-
-// const Auth = ( {children} ) => {
-
-//     define reducer
-//     define useEffect to run on every render
-
-//     define authContext = {
-//         signIn: (email, password) => {
-//             POST details to BE, get JWT
-//             dispatch new details to reducer
-//         },
-//         signOut: () => {
-//             remove JWT from browser localStorage
-//             dispatch SIGN OUT action
-//         },
-//         user: state.user
-//     }
-
-//     return (
-//         <AuthContext.Provider value={authContext}>{children}</AuthContext.Provider>
-//       )
-// }
