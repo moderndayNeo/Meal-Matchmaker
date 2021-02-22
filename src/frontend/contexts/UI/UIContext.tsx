b4f15fd72eb7d5ddb9a9ca2fcb59edd47d6a233d
Adam Zdrzalka <Adam.JZ@outlook.com>
2021-02-22 10:34:06 +0000

    refactor: add setSignInModal function to UIContext

--- src/frontend/contexts/UI/UIContext.tsx
+++ src/frontend/contexts/UI/UIContext.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
-interface UIContextInterface {
-    signInModal: boolean
-}
+// interface UIContextInterface {
+//     signInModal: boolean,
+//     setSignInModal: () => void
+// }
 
-const defaultUIContext: UIContextInterface = {
-    signInModal: true,
-}
+// const defaultUIContext: UIContextInterface = {
+//     signInModal: true,
+//     setSignInModal
+// }
 
-const UIContext = React.createContext<UIContextInterface>(defaultUIContext)
+// const UIContext = React.createContext<UIContextInterface>(defaultUIContext)
 
+const UIContext = React.createContext<any>({})
 export default UIContext
--- src/frontend/contexts/UI/UIContextProvider.tsx
+++ src/frontend/contexts/UI/UIContextProvider.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 import UIContext from './UIContext'
 
 const UI: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    const [signInModal, setSignInModal] = React.useState<boolean>(true)
+
     const uiContext = {
-        signInModal: true,
+        signInModal,
+        setSignInModal,
     }
 
     return <UIContext.Provider value={uiContext}>{children}</UIContext.Provider>
