0b8623abd65325cc8677be127e97b170c1576110
Adam Zdrzalka <Adam.JZ@outlook.com>
2021-02-22 10:52:14 +0000

    feat: Only save recipe if user is logged in

--- src/frontend/components/common/SaveRecipeButton/SaveRecipeButton.tsx
+++ src/frontend/components/common/SaveRecipeButton/SaveRecipeButton.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
 import './SaveRecipeButton.css'
 import { ReactComponent as UnfilledStarIcon } from '../../../media/Icons_star_unfilled.svg'
+import AuthContext from '../../../contexts/Auth/AuthContext'
+import UIContext from '../../../contexts/UI/UIContext'
 
 export default function SaveRecipeButton() {
-    return <UnfilledStarIcon className="SaveRecipeButton" />
+    const { isLoggedIn } = React.useContext(AuthContext)
+    const { setSignInModal } = React.useContext(UIContext)
+
+    const handleClick = () => {
+        if (isLoggedIn) {
+            // save recipe
+        } else {
+            // show sign in modal
+            setSignInModal(true)
+        }
+    }
+
+    return (
+        <UnfilledStarIcon className="SaveRecipeButton" onClick={handleClick} />
+    )
 }
