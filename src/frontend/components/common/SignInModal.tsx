dcb4ea42ff6fc09c87ed2ee88870ffde27aa5ecb
Adam Zdrzalka <Adam.JZ@outlook.com>
2021-02-22 10:35:04 +0000

    feat: Style SignInModal

--- src/frontend/components/common/SignInModal.tsx
+++ src/frontend/components/common/SignInModal.tsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import './SignInModal.css'
 import { Link } from 'react-router-dom'
-// import UIContext from '../../contexts/UI/UIContext'
+import UIContext from '../../contexts/UI/UIContext'
 
 export default function SignInModal() {
-    // const { setSignInModal } = React.useContext(UIContext)
+    const { setSignInModal } = React.useContext(UIContext)
 
     return (
         <div className="SignInModal">
             <div className="modal">
-                <p>Sign In to save your favourite recipes</p>
+                <p className="title">Sign In to save your favourite recipes</p>
                 <Link to="/login" className="login-link generate-button">
                     Sign In
                 </Link>
                 <p
                     className="cancel-button"
-                    // onClick={setSignInModal(false)}
+                    onClick={() => setSignInModal(false)}
                 >
                     Cancel
                 </p>
